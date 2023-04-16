42d14f469a414c3f772ef65952c0b043a58ca8a0
Muhammad <usmanehsanullah3644@gmail.com>
2023-04-16 08:50:19 +0500

    fix some bugs

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Banner = () => {
   return (
     <>
-    <div className="bigh text-center font-semibold lg:text-[270px] text-[55px] text-[#EDECE8]   ">
+    <div className="bigh text-center font-semibold lg:text-[230px] text-[55px] text-[#EDECE8]   ">
         <h1>All Works</h1>
     </div>
     
